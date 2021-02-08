498449e8be331e02eb9ad2d0e29cf6305b88592d
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-08 16:37:04 +0900

    feature: Created 'Add ToDo Item Form' Functionality

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,46 @@
+import { useState } from "react"
 import './App.css';
 
 function App() {
+
+  // JavaScript
+
+  const [todos, setToDos] = useState([
+    "Work on the TeamLab Challenge",
+    "Workout in Gym",
+    "Learn ReactJS",
+  ])
+
+  const [input, setInput] = useState('')
+
+  // Function : Add ToDo Task Item
+  const addToDo = (event) => {
+
+    event.preventDefault() /* Stops the Browser REFRESHING Functionality */
+
+    setToDos([ ...todos, input ])
+    setInput('') /* Clear up the input field */
+  }
+
+
   return (
+
+    // JSX
+
     <div className="App">
-      
+      <h1>ToDo Application</h1>
+
+      <form>
+        <input value={ input } onChange={ event => setInput(event.target.value) }/>
+
+        <button type="submit" onClick={ addToDo }>Add ToDo Item</button>
+      </form>
+
+      <ul>
+        { todos.map( todo => (
+          <li>{ todo }</li>
+        ) ) }
+      </ul>
     </div>
   );
 }
